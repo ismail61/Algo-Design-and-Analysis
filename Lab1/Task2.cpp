ee927e9d733ef6f05a8d03a90fcd754e5cecbb18
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2019-07-14 00:45:29 -0700

    Add files via upload

---
+++ Lab1/Task2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long m = 100000000;
+int Hash(string s, int radix, long long m)
+{
+        int r = s.length();
+        int hash = 0;
+        int i, j;
+        for(i=r-1; i>=0; --i)
+        {
+                int ans = s[i];
+                for(j=r-i-1; j>0; --j)
+                {
+                        ans *= radix;
+                        ans %= m;
+                }
+                hash += ans;
+                hash %= m;
+        }
+        return hash;
+}
+int main()
+{
+
+    int radix;
+    string s;
+    cin>>radix;
+    set<int>v;
+    int i=0;
+    while(i!=5){
+            cin>>s;
+            long long hi = Hash(s,radix,m);
+            //cout<<hi<<endl;
+            v.insert(hi);
+            i++;
+    }
+    cout<<v.size();
+
+}
