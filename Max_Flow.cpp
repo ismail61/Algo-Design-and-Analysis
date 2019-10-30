1aa2fedd38d9e037633a97faba1b10c3b476a14d
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2019-10-31 01:54:20 +0600

    Add files via upload

--- Max_Flow.cpp
+++ Max_Flow.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define V 6
-bool bfs(int rgraph[V][V],int s,int t,int parent[V]){
+bool bfs(int rgraph[V][V],int s,int t,int parent[V])
+{
     bool visited[V];
     memset(visited,0,sizeof(visited));
     queue<int>q;
     q.push(s);
     visited[s]=true;
     parent[s]=-1;
-    while(!q.empty()){
+    while(!q.empty())
+    {
         int u = q.front();
         q.pop();
 
-        for(int v=0;v<V;v++){
-            if(visited[v]==false && rgraph[u][v]>0){
+        for(int v=0; v<V; v++)
+        {
+            if(visited[v]==false && rgraph[u][v]>0)
+            {
                 q.push(v);
                 parent[v]=u;
                 visited[v]=true;
@@ -22,30 +26,61 @@ bool bfs(int rgraph[V][V],int s,int t,int parent[V]){
     }
     return (visited[t] == true);
 }
-int fordFulkerson(int graph[V][V],int s,int t){
+int fordFulkerson(int graph[V][V],int s,int t)
+{
     int rgraph[V][V];//residual graph
     int u,v;
-    for(u=0;u<V;u++){
-        for(v=0;v<V;v++){
+    for(u=0; u<V; u++)
+    {
+        for(v=0; v<V; v++)
+        {
             rgraph[u][v]=graph[u][v];
         }
     }
     int parent[V];
     int max_flow = 0;
-    while(bfs(rgraph,s,t,parent)){
+    vector<int>vv;
+    vector<int>vvv;
+    int high = 0;
+    while(bfs(rgraph,s,t,parent))
+    {
         int path_flow = INT_MAX;
-        for(int v=t;v!=s;v=parent[v]){
+        cout<<endl;
+        vv.clear();
+        cout<<"path is : ";
+        for(int v=t; v!=s; v=parent[v])
+        {
+
             int u=parent[v];
+            vv.push_back(v);
+            cout<<v<<" ";
             path_flow = min(path_flow,rgraph[u][v]);
+
         }
-        cout<<path_flow<<" : ";
-        for(v=t;v!=s;v=parent[v]){
+        cout<<"0  & path flow value is "<<path_flow<<" "<<endl;
+        if(path_flow>high)
+        {
+            high = path_flow;
+            vvv.clear();
+            for(int is = 0; is <vv.size(); is++)
+            {
+                vvv.push_back(vv[is]);
+            }
+        }
+        for(v=t; v!=s; v=parent[v])
+        {
             u=parent[v];
             rgraph[u][v] -= path_flow;
             rgraph[v][u] += path_flow;
         }
         max_flow += path_flow;
     }
+    cout<<"maximum path is : "<<high<<" & path size is: "<<vvv.size()<< " & path is : ";
+    for(int i=0; i<vvv.size(); i++)
+    {
+        cout<<vvv[i]<< " ";
+    }
+    cout<<endl;
     return max_flow;
 }
 int main()
